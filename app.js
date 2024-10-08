3e30f0a3e57c470b33e81ae1b169b8a4d5691cec
Ixrael <olutokijuwon12@gmail.com>
2024-10-08 15:25:16 +0100

    Added other features

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+const data = fs.readFileSync(`${__dirname}/data/todo.json`, 'utf8');
+
 //Getting request for all Todos
 app.get('/todos', (req, res) => {
-  res.send('Welcome to the server!');
+  res.status(200).json({
+    status: 'Success',
+    data: JSON.parse(data),
+  });
+});
+
+app.post('/todos', (req, res) => {
+  const newTodo = Object.assign();
 });
 
 //Listening to port
